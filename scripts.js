e1aa1db9ce17a737d4d2bfa1dd5e1df906b32f90
hsin0518 <skes40216@gmail.com>
2025-04-02 10:58:25 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// This script can be expanded later for any interactions you want to add
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Kabo’s Adventure page loaded');
+});
